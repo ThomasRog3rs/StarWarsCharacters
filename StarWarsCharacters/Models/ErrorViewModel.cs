ecc5cbb2856270b50eb56a77eb8f721acbb7f355
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2022-06-21 15:01:41 +0100

    Add project files.

---
+++ StarWarsCharacters/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StarWarsCharacters.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
